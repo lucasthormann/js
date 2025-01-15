871bdee148a40aeffa4a2b0ea2a7049b2a757e09
lucasthormann <LucasThormann@protonmail.com>
2025-01-15 16:10:30 -0600

    template literals

---
+++ templateliterals.js
@@ -0,0 +1,14 @@
+const person = {
+    name: "Zodiac Hasbro",
+    age: 56
+};
+
+// template literal with multi-line and string interpolation
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
+
+const result = {
+    success: []
+}

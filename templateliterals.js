fe1178f009e1c722a25782ae8edcc6cff453b3b0
lucasthormann <LucasThormann@protonmail.com>
2025-01-16 15:33:07 -0600

    more on template literals

--- templateliterals.js
+++ templateliterals.js
@@ -10,5 +10,19 @@ I am ${person.age} years old.`;
 console.log(greeting);
 
 const result = {
-    success: []
+    success: ["max-lengt", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+};
+
+function makeList(arr){
+    const resultDisplayArray = [];
+    for(let i = 0; i < arr.length; i++){
+        resultDisplayArray.push(`<li class="test-warning">${arr[i]}</li>`)
+    }
+
+return resultDisplayArray;
 }
+
+const resultDisplayArray = makeList(result.failure);
+console.log(resultDisplayArray);
